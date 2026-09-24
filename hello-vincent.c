168c7a234c66e9ef18e850641d09fe0402ef32d6
Vincent365608 <vincentomunga7@gmail.com>
2026-09-24 10:40:24 +0300

    Add hello-vincent.c to print course information
    Implement a C program to display course names.

---
+++ hello-vincent.c
@@ -0,0 +1,20 @@
+single line comment
+//program to display 'Hello world'
+//multiple line comment
+*/
+pre-processor directives
+#include<stdio.h>
+int main(){
+    printf("UCU2107 Fundamentals Of Digital Technology ArtificialInteligent\n");
+    printf("SPI2103 Introduction To Computer Application\n");
+    printf("SPI2120 Essentials Of Accounting\n");
+    printf("SPI2102 Computer System AndOrganization\n");
+    printf("SPH2107 Analogue Electronics\n");
+    printf("SPM2104 Mathemtical For Science\n");
+    printf("SPI2101 Introduction To Progroming AndAlgoriths\n");
+    printf("UCU2101 Communication And Information Litracy Skills\n");
+    return 0;
+    hello-3.C
+    
+    
+}
